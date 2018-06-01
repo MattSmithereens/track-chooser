b4e4a3f8ff81203ca1d71d941ceaf8ebe9cea3bc
Matt Smith <mattsmithereens@gmail.com>
2018-06-01 10:08:30 -0700

    add minor code tweaks, no additional functionality

--- Desktop/course-chooser/js/scripts.js
+++ Desktop/course-chooser/js/scripts.js
@@ -13,18 +13,19 @@ $(document).ready(function() {
     var answer3 = parseInt($("input:radio[name=q3]:checked").val());
     var answer4 = parseInt($("input:radio[name=q4]:checked").val());
     var answer5 = parseInt($("input:radio[name=q5]:checked").val());
-    var result = (answer1+answer2+answer3+answer4+answer5)
+    var result = (answer1+answer2+answer3+answer4+answer5);
+    console.log(result);
 
 
-    if (result <= 5) {
+    if (result <= 7) {
       hideAll();
       $("#css").show();
       result.empty();
-    } else if (result > 5 && result <= 10) {
+    } else if (result > 8 && result <= 14) {
       hideAll();
       $("#cSharp").show();
       result.empty();
-    } else if (result > 10 && result <= 20) {
+    } else if (result > 14 && result <= 21) {
       hideAll();
       $("#php").show();
       result.empty();
